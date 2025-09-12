321f41162012110208d2dbd3dbbc7fdf77ae3b16
Romain <163823947+romaindenis1@users.noreply.github.com>
2025-09-12 15:04:11 +0200

    feat(Form1.cs): make base graph with random data hard inserted
    [75mins][DONE]

---
+++ Plot_Those_Lines/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Plot_Those_Lines
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.formsPlot1 = new ScottPlot.WinForms.FormsPlot();
+            this.SuspendLayout();
+            // 
+            // formsPlot1
+            // 
+            this.formsPlot1.DisplayScale = 0F;
+            this.formsPlot1.Location = new System.Drawing.Point(2, 0);
+            this.formsPlot1.Name = "formsPlot1";
+            this.formsPlot1.Size = new System.Drawing.Size(1342, 697);
+            this.formsPlot1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1341, 764);
+            this.Controls.Add(this.formsPlot1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ScottPlot.WinForms.FormsPlot formsPlot1;
+    }
+}
+
---
+++ Plot_Those_Lines/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CsvHelper;
+using CsvHelper.Configuration;
+namespace Plot_Those_Lines
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+                InitializeComponent();
+                double[] dataX = years.ToArray();
+
+                //formsPlot1.Plot.Clear();
+
+                foreach (var kvp in data)
+                {
+                    double[] dataY = kvp.Value.ToArray();
+                    var scatter = formsPlot1.Plot.Add.Scatter(dataX, dataY);
+                    scatter.Label = kvp.Key;
+                }
+
+                formsPlot1.Plot.Title("Player Heights by Position Over Years");
+                formsPlot1.Plot.XLabel("Year");
+                formsPlot1.Plot.YLabel("Height");
+                formsPlot1.Plot.Legend.IsVisible = true;
+
+                formsPlot1.Refresh();
+
+            }
+            finally
+            {
+                reader.Dispose();
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
